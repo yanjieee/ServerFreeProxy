9b5721b677d80ce2e6b383170cea8fc788d9a742
yanjieee <yanjieee@gmail.com>
2014-08-24 20:34:44 +0800

    first commit

---
+++ ServerFreeProxy/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ServerFreeProxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "11",
+            "111.111.111.111",
+            "2014年11月11日 11:11:11",
+            "asdfasdfasdfsdfsdfsfsdfsdfasdfasdfsdfds"}, -1);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listView1.GridLines = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(817, 477);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "";
+            this.columnHeader4.Width = 30;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Width = 138;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "时间";
+            this.columnHeader2.Width = 214;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "报告";
+            this.columnHeader3.Width = 393;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 501);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ ServerFreeProxy/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ServerFreeProxy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyServer myServer = new MyServer();
+            myServer.onReport += new MyServer.receiveReportDelegate(onGetReport);
+            Thread th = new Thread(myServer.run);
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private void onGetReport(string report)
+        {
+            if (this.IsHandleCreated)
+            {
+                this.BeginInvoke((Action)delegate()
+                {
+                    string[] str = report.Split('|');
+                    ListViewItem tmpItem = null; 
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        if (str[0].Equals(item.SubItems[1].Text))
+                        {
+                            item.Remove();
+                            break;
+                        }
+                    }
+                    if (tmpItem == null)
+                    {
+                        tmpItem = new ListViewItem();
+                        tmpItem.SubItems.Add(str[0]);
+                        tmpItem.SubItems.Add(str[1]);
+                        tmpItem.SubItems.Add(str[2]);
+                        listView1.Items.Add(tmpItem);
+                    }
+                    for (int i = 0; i < listView1.Items.Count; i++)
+                    {
+                        listView1.Items[i].SubItems[0].Text = (i + 1).ToString();
+                    }
+                });
+                
+            }
+        }
+
+        class MyServer
+        {
+            private UdpClient _server;
+
+            public MyServer()
+            {
+                _server = new UdpClient(3000);
+            }
+
+            public delegate void receiveReportDelegate(string report);
+            public event receiveReportDelegate onReport;
+
+            public void run()
+            {
+                while(true)
+                {
+                    IPEndPoint receivePoint = new IPEndPoint(IPAddress.Any, 3000);
+                    byte[] recData = _server.Receive(ref receivePoint);
+                    //Console.WriteLine(receivePoint.Address.ToString() + Encoding.ASCII.GetString(recData));
+                    string report = Encoding.ASCII.GetString(recData);
+                    if (report.Contains("[FreeProxy]"))
+                    {
+                        if (onReport != null)
+                        {
+                            onReport(receivePoint.Address.ToString() + "|" + DateTime.Now.ToString() + "|" + report.Replace("[FreeProxy]", ""));
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
